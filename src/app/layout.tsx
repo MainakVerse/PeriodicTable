e106badb28774d09917578b5d096c4eadb66209e
MAINAK CHAUDHURI <mainakchaudhuri671@gmail.com>
2025-04-07 14:36:27 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Poppins, Satisfy, Lora } from "next/font/google";
 import ThemeProvider from "@/context/ThemeProvider";
 
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["400"],
-  style: "normal",
-  variable: "--font-poppins",
-});
-
-const satisfy = Satisfy({
-  weight: "400",
-  subsets: ["latin"],
-  style: "normal",
-  variable: "--font-satisfy",
-});
-
-const lora = Lora({
-  weight: ["400", "500", "600", "700"],
-  style: ["normal", "italic"],
-  subsets: ["latin"],
-  variable: "--font-lora",
-});
-
 export const metadata: Metadata = {
-  metadataBase: new URL(
-    `https://${process.env.VERCEL_URL}` ||
-      `http://localhost:${process.env.PORT || 3000}`
-  ),
   title: "Modern Period Table",
   description:
     "Elemental Explorer is an innovative and interactive platform that reimagines the traditional periodic table for the digital age. With an intuitive interface and comprehensive details on each element, this modern periodic table provides an engaging educational experience for students, educators, and science enthusiasts. Explore atomic properties, electron configurations, historical data, and practical applications with ease. Elemental Explorer brings chemistry to life, making the study of elements more accessible and fascinating than ever before.",
@@ -90,9 +64,7 @@ export default function RootLayout({
   return (
     <ThemeProvider>
       <html lang="en">
-        <body
-          className={`${poppins.variable} ${satisfy.variable} ${lora.variable}`}
-        >
+        <body className="font-sans">
           {children}
         </body>
       </html>
